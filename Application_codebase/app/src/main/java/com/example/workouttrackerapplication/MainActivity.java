6e76a037283cc77899b1af86c0ee5874a79deaef
Joshua Green <19026968@brookes.ac.uk>
2023-11-08 17:16:10 +0000

    implemented new icon for 'workouts' on nav bar

--- Application_codebase/app/src/main/java/com/example/workouttrackerapplication/MainActivity.java
+++ Application_codebase/app/src/main/java/com/example/workouttrackerapplication/MainActivity.java
@@ -12,6 +12,7 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.workouttrackerapplication.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,12 +28,15 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
+
+
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_workouts)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
     }
 
 }
