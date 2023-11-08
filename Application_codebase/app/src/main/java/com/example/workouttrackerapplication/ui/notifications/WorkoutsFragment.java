a093e6b85b1a695159d9fb102be76070bd022c7e
Joshua Green <19026968@brookes.ac.uk>
2023-11-08 14:51:22 +0000

    changed notification fragment file name

--- Application_codebase/app/src/main/java/com/example/workouttrackerapplication/ui/notifications/WorkoutsFragment.java
+++ Application_codebase/app/src/main/java/com/example/workouttrackerapplication/ui/notifications/WorkoutsFragment.java
@@ -12,7 +12,7 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.workouttrackerapplication.databinding.FragmentNotificationsBinding;
 
-public class NotificationsFragment extends Fragment {
+public class WorkoutsFragment extends Fragment {
 
     private FragmentNotificationsBinding binding;
 
